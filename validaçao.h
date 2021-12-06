9ed41e84d810eb047361c0b8e4be4aba0d1b50ac
francimar01 <92753716+francimar01@users.noreply.github.com>
2021-12-06 15:42:18 -0300

    Add files via upload

---
+++ validaçao.h
@@ -0,0 +1,9 @@
+#ifndef VALIDAÁAO_H_INCLUDED
+#define VALIDAÁAO_H_INCLUDED
+
+int placa (void);
+void lerPlaca(char placa[]);
+void validaPlaca(char placa[]);
+void isnum(char caracter);
+
+#endif // VALIDAÁAO_H_INCLUDED
