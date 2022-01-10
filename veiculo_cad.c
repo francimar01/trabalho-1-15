cd096fe91ad379ed7f29e36e841641052dd33ecc
francimar01 <92753716+francimar01@users.noreply.github.com>
2022-01-10 13:35:45 -0300

    Add files via upload

--- veiculo_cad.c
+++ veiculo_cad.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "veiculo_cad.h"
+#include "veiculos_cad.h"
+#include "cadastrar.h"
 
 void veiculos_cad (void){
+    for(i=0;i<1;i++){
 	system ("limpar || cls");
-	printf("\tTELA  VEICULO cadastrados\n");
+	printf("\tTELA  VEICULO CADASTRADOS\n");
 	printf("\t======================\n");
+	printf("PLACA:  %s\n",vei_cadastrados[i].PLACA);
+	printf("MODELO:  %s\n",vei_cadastrados[i].MARCA);
+	printf("COR:  %s\n",vei_cadastrados[i].COR);
 	getchar();
 	system ("pause");
+    }
 }
---
+++ veiculos_cad.h
@@ -0,0 +1,7 @@
+#ifndef VEICULOS_CAD_H_INCLUDED
+#define VEICULOS_CAD_H_INCLUDED
+
+void veiculo_cad (void);
+
+
+#endif // VEICULOS_CAD_H_INCLUDED
