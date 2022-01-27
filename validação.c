7d0d2d681ed22dceb62066f878d676ca061eb948
francimar01 <92753716+francimar01@users.noreply.github.com>
2022-01-26 22:00:04 -0300

    Add files via upload
    validar placa

---
+++ validação.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "validação.h"
+
+int valida(char *placa){
+    if(strlen(placa) != 7){
+        return 0;
+    }
+    for(int i = 0; placa[i] != 0; i += 1){
+        if(i <= 2){
+            if(!isalpha(placa[i])){
+                return 0;
+            }
+        }else{
+            if(!isdigit(placa[i])){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
---
+++ validação.h
@@ -0,0 +1,6 @@
+#ifndef VALIDAÁ„O_H_INCLUDED
+#define VALIDAÁ„O_H_INCLUDED
+
+int valida(char*);
+
+#endif // VALIDAÁ„O_H_INCLUDED
