749ee7c4002631d80c95cd1f0f9155db49c1328b
francimar01 <92753716+francimar01@users.noreply.github.com>
2022-02-10 23:15:02 -0300

    Rename validação.h to validaçao.h

---
+++ validaçao.h
@@ -0,0 +1,6 @@
+#ifndef VALIDAçãO_H_INCLUDED
+#define VALIDAçãO_H_INCLUDED
+
+int valida(char*);
+
+#endif // VALIDAçãO_H_INCLUDED
--- validação.h
+++
@@ -1,6 +0,0 @@
-#ifndef VALIDAÁ„O_H_INCLUDED
-#define VALIDAÁ„O_H_INCLUDED
-
-int valida(char*);
-
-#endif // VALIDAÁ„O_H_INCLUDED
